59b9dfdf0690f4ab470df369c70061397ae7a97b
wenguz <wenguz1518@gmail.com>
2019-03-10 19:30:18 -0400

    BLADE: embebimos vista de master en hola mundo

--- resources/views/hola-mundo.blade.php
+++ resources/views/hola-mundo.blade.php
@@ -1,1 +1,13 @@
-'Hola mundo: Bienvenido a laravel'
+@extends('layouts.master')
+ <!--esto aparecera en la pestaña de la pagina-->
+	@section('title','Curso Laravel Basico')
+<!--Para heredar algo mas al header que tenia el master-->
+@section('header')
+	@parent
+	<h1>cabecera desde hola mundo</h1>
+@stop
+
+<!--Contenido:se reemplazo el contenido que venia del master-->
+@section('content')
+	<p>Contenido de hola munod</p>
+@stop
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -2,6 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
+        <!--esto aparecera en la pestaña de la pagina-->
         <title>Probando Balde - @yield('title')</title>
     </head>
     <body>
