2459febed61970ca5ca2714d11c526afb813d311
wenguz <wenguz1518@gmail.com>
2019-03-10 18:26:23 -0400

    BLADE: formas de imprimir datos en blade

--- resources/views/contacto/contacto.blade.php
+++ resources/views/contacto/contacto.blade.php
@@ -1,1 +1,8 @@
-<h1>Pagina de contacto{{$nombre}}  {{$edad}}</h1>
+<!--Forma basica para mostrar informacion en blade-->
+<h1>Pagina de contacto {{$nombre}}  {{$edad}}</h1>
+
+<!--otra forma para mostrar informacion en blade-->
+<h1>Pagina de contacto otro {!!$nombre!!}  {!!$edad!!}</h1>
+
+<!--otra forma para mostrar informacion en blade con php-->
+<h1>Pagina de contacto con php <?=$nombre?>  <?=$edad?></h1>
