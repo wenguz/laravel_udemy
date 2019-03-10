1647788708dd395ad323b0b6b592a6b16aa09d69
wenguz <wenguz1518@gmail.com>
2019-03-10 19:47:19 -0400

    PROYECTO TIENDA:php artisan migrate

--- database/migrations/2019_03_10_233323_create_products_table.php
+++ database/migrations/2019_03_10_233323_create_products_table.php
@@ -15,6 +15,8 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->text('description');
+            $table->integer('price');
             $table->timestamps();
         });
     }
