d2031056545008053fe17b62f58570a9fc6d1dc4
wenguz <wenguz1518@gmail.com>
2019-03-10 20:13:08 -0400

    PROYECTO TIENDA: creacion de vista index dentro de products con diseño bottstrap

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -2,8 +2,25 @@
 <html>
 <head>
 	<title></title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
-<h1>Listado de Productos</h1>
+<div class="container">
+	<div class="row">
+		<div class="col-md-12">
+			<div class="card">
+			<!--inicio titulo-->
+				<div class="card-header">
+					Listado de productos
+					<a href="" class="btn btn-success btn-sm float-right">Nuevo producto</a>
+				</div>
+			<!--fin titulo-->
+				<div class="card-body">
+				......
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
 </body>
 </html>
